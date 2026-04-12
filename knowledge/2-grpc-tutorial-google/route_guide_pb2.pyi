27ce096e8ebab304cde6d2875ee99991abc88ee3
Kamil Kulig <kamil.marek.kulig@gmail.com>
2026-04-12 15:27:25 +0200

    #22 chore: generete biolerpate code, interfaces we defined in route_guide.proto

---
+++ knowledge/2-grpc-tutorial-google/route_guide_pb2.pyi
@@ -0,0 +1,25 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Point(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class Rectangle(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class Feature(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class RouteNote(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class RouteSummary(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
